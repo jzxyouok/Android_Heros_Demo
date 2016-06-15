a2e9bedb60b4e519e34ddc7e60e82fa879a4503a
欢颜 <wl376705253@163.com>
2016-06-15 10:45:30 +0800

    cusTextview

---
+++ chapter_Three/src/main/java/simple_demo/CusTextView.java
@@ -0,0 +1,46 @@
+package simple_demo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by eagle on 2016/6/15.
+ */
+public class CusTextView extends TextView{
+    private Paint mPaint1,mPaint2;
+    public CusTextView(Context context) {
+        this(context,null);
+    }
+
+    public CusTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CusTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    private void initView() {
+        mPaint1 = new Paint();
+        mPaint1.setColor(Color.parseColor("#ffaabb"));
+        mPaint1.setStyle(Paint.Style.FILL);
+        mPaint2 = new Paint();
+        mPaint2.setColor(Color.YELLOW);
+        mPaint2.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawRect(0,0,getMeasuredWidth(),getMeasuredHeight(),mPaint1);
+        canvas.drawRect(10,10,getMeasuredWidth()-10,getMeasuredHeight()-10,mPaint2);
+        canvas.save();
+        canvas.translate(10,0);
+        super.onDraw(canvas);
+        canvas.restore();
+    }
+}
