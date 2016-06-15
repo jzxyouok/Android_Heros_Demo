4dc1f509a11f035d48f7457c37d0b48afdb95d0d
欢颜 <wl376705253@163.com>
2016-06-15 14:16:36 +0800

    VolumnView

--- chapter_Three/src/main/java/com/eaglei/android_heros_demo/SecondActivity.java
+++ chapter_Three/src/main/java/com/eaglei/android_heros_demo/SecondActivity.java
@@ -1,32 +1,27 @@
 package com.eaglei.android_heros_demo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import simple_demo.CombounView;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
     CombounView combounView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         combounView = (CombounView) findViewById(R.id.topbar);
         combounView.setOnToobarClickListener(new CombounView.TopbarClickListener() {
             @Override
             public void leftClick() {
-                Toast.makeText(MainActivity.this,"left",Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
             public void rightClick() {
-                Intent mIntent = new Intent(MainActivity.this,SecondActivity.class);
-                startActivity(mIntent);
+
             }
         });
-        combounView.setButtonVisable(0,false);
-
     }
 }
