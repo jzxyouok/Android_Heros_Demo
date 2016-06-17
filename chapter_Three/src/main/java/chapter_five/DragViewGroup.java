a0f87aa6c17516bd90164ed73ec63c4334f533f2
欢颜 <wl376705253@163.com>
2016-06-17 13:49:22 +0800

    dragViewGroup

---
+++ chapter_Three/src/main/java/chapter_five/DragViewGroup.java
@@ -0,0 +1,103 @@
+package chapter_five;
+
+import android.content.Context;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * Created by eagle on 2016/6/17.
+ */
+public class DragViewGroup extends FrameLayout{
+
+    private ViewDragHelper mHelper;
+    private View mMenuView, mMainView;
+    int width;
+
+
+
+    public DragViewGroup(Context context) {
+        this(context, null);
+    }
+
+    public DragViewGroup(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DragViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    private void initView() {
+        mHelper = ViewDragHelper.create(this , callback);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mMenuView = getChildAt(0);
+        mMainView = getChildAt(1);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = mMenuView.getMeasuredWidth();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return mHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mHelper.processTouchEvent(event);
+        return true ;
+    }
+
+    private ViewDragHelper.Callback callback = new ViewDragHelper.Callback() {
+        @Override
+        public boolean tryCaptureView(View child, int pointerId) {
+            return mMainView == child;
+        }
+
+        @Override
+        public void onViewCaptured(View capturedChild, int activePointerId) {
+            super.onViewCaptured(capturedChild, activePointerId);
+        }
+
+        @Override
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            return 0 ;
+        }
+
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            return left;
+        }
+
+        @Override
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            super.onViewReleased(releasedChild, xvel, yvel);
+            if (mMainView.getLeft() < 500){
+                mHelper.smoothSlideViewTo(mMainView, 0 , 0);
+                ViewCompat.postInvalidateOnAnimation(DragViewGroup.this);
+            }else{
+                mHelper.smoothSlideViewTo(mMainView , 300 ,0);
+                ViewCompat.postInvalidateOnAnimation(DragViewGroup.this);
+            }
+        }
+    };
+
+    @Override
+    public void computeScroll() {
+        if (mHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+    }
+}
--- chapter_Three/src/main/java/com/eaglei/android_heros_demo/ScrollActi.java
+++ chapter_Three/src/main/java/com/eaglei/android_heros_demo/ScrollActi.java
@@ -1,13 +1,26 @@
 package com.eaglei.android_heros_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import chapter_five.DragView3;
 
 public class ScrollActi extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll);
+        button = (Button) findViewById(R.id.btn_int);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollActi.this , ScrollViewGroupActi.class);
+                startActivity(intent);
+            }
+        });
     }
 }
