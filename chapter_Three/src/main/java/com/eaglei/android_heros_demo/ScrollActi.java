10eb5e654c8a8b1c9c79c1293b81974b79bd0862
欢颜 <wl376705253@163.com>
2016-06-21 09:46:04 +0800

    delete

--- chapter_Three/src/main/java/com/eaglei/android_heros_demo/ScrollActi.java
+++ chapter_Three/src/main/java/com/eaglei/android_heros_demo/ScrollActi.java
@@ -3,6 +3,7 @@ package com.eaglei.android_heros_demo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 
